96a557337c4633a3b719ad638b5cbc0edddd721f
Satoshi Mimura <mimura@smegandsynaps.jp>
2025-02-04 19:35:48 +0900

    update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   firebase: {
-    apiKey: 'IzaSyD3v_19Cus_IAd2zKzmG1U7JngXiSQkNCw"',
-    authDomain: 'friendlychat-3df0d.firebaseapp.com"',
+    apiKey: 'IzaSyD3v_19Cus_IAd2zKzmG1U7JngXiSQkNCw',
+    authDomain: 'friendlychat-3df0d.firebaseapp.com',
     databaseURL: 'https://friendlychat-3df0d.firebaseio.com',
     projectId: 'friendlychat-3df0d',
     storageBucket: 'friendlychat-3df0d.firebasestorage.app',
